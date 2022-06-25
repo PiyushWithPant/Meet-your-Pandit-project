cd7e312aacf176ef490b6ef11df0b068c96d21f9
PIYUSH PANT <piyushpant15@gmail.com>
2022-06-25 22:44:32 +0530

    Server Initiated

--- app.js
+++ app.js
@@ -4,6 +4,44 @@ if (process.env.NODE_ENV !== 'production') {
 
 // Modules required
 
+const express = require('express');
+const app = express();
+
+// our boilerplate engine
+const engine = require('ejs-mate');
+
+app.engine('ejs', engine);
+
+const path = require('path');
+
+const mongoose = require('mongoose');
+
+
+// Database Connection
+const dbUrl = process.env.MONGODB_URL || "mongodb://localhost:27017/pujaApp"
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+    console.error.bind(console, "Connection Error :-(")
+});
+
+db.once('open', () => {
+    console.log("Connected to Database...")
+});
+
+
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// to serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
