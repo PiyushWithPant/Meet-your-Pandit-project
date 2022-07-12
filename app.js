83fb67b25cf46ef4352202ff7bcfcfc631969794
PIYUSH PANT <piyushpant15@gmail.com>
2022-07-12 16:21:12 +0530

    Resolving error

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// File Upload Packages
+const fileUpload = require('express-fileupload')
+// Use v2
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
+
 // Rest of the packages
 
 const morgan = require('morgan'); // to check the status of each request
@@ -53,26 +64,11 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
-
-
-
-
-
-
 app.use(morgan('tiny'));
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(cors());
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Home routes
