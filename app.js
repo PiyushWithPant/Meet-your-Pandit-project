408d399c8ce53c0bfc6372083cf9272f32f2a56d
PIYUSH PANT <86486393+Piyush15072002@users.noreply.github.com>
2022-07-12 16:08:00 +0530

    Updated

--- app.js
+++ app.js
@@ -55,37 +55,6 @@ app.use(morgan('tiny'));
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(cors());
 
-// poojas - temporary
-
-poojasList = [
-    'Ganapathi Homam',
-    'Seemantham (Baby Shower)',
-    'Wedding (Vivaham)',
-    'Punyaha Vachanam and Nama Karanam',
-    'Anna Prasanam',
-    'Chowla Upanayanam / Thread Ceremony',
-    'Ugra Radha Shanti / 59th Birthday',
-    'Shashti Purti (Shastabya Purti / Shashtiabdha Poorthi / 60th Birthday)',
-    'Vijaya Radha Shanti / 75th Birthday',
-    'Sata Abhishekam / Sathabhishekam / 80th Birthday',
-    'Purna Kanaka Abhishekham',
-    'Rudra Abhishekam',
-    'Navagraha Homam',
-    'Ayushya Homam / Mrityunjaya Homam',
-    'Chandi Homam',
-    'Sudharshana Homam',
-    'Sri Maha Lakshmi Homam Sri Suktha Homam',
-    'Durga Homam / Lakshmi Homam / Saraswati Homam',
-    'Bhagavaty Seva',
-    'Vaasthu Homam / Gruha Pravesh',
-    'Nakshatra Homam',
-    'Satya Narayana Vratham / Satya Narayana Puja',
-    'Ayyappa Puja',
-    'Bhima ratha Shanthi (70th Birthday)',
-    'Bhoomi Pooja',
-    'Kanakabhishekam',
-    'Laskshmi Kubera Pooja',
-]
 
 
 // Home routes
