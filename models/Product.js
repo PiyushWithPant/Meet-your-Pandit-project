acfe648d13692217e080047cfcf609a7d7dba4fd
Sambhav Aggarwal <sambhav.11111@gmail.com>
2022-07-20 22:19:57 +0530

    Bug Fixes

--- models/Product.js
+++ models/Product.js
@@ -33,30 +33,16 @@ const ProductSchema = new mongoose.Schema({
         numOfReviews: {
             type: Number,
             default: 0,
-        }
-        // user: {
-        //     type: mongoose.Types.ObjectId,
-        //     ref: 'User',
-        //     required: true,
-        // },
+        },
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
     }, {
         timestamps: true,
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
     },
 
 );
-
-// ProductSchema.virtual('reviews', {
-//     ref: 'Review',
-//     localField: '_id',
-//     foreignField: 'product',
-//     justOne: false,
-//     //match: {rating: 5}
-// });
-
-// ProductSchema.pre('remove', async function(next) {
-//     await this.model('Review').deleteMany({ product: this._id })
-// });
 
 module.exports = mongoose.model('Product', ProductSchema);
