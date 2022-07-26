7f5db8c07fa11edb1e9b07c39d380805e44130ef
Sambhav Aggarwal <sambhav.11111@gmail.com>
2022-07-26 18:57:20 +0530

    Update panditController.js

--- controllers/panditController.js
+++ controllers/panditController.js
@@ -2,9 +2,9 @@ const Pandit = require('../models/Pandit')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const { createTokenUser, attachCookiesToResponse } = require('../utils')
-// Admin Controller to Display information of every pandit
+    // Admin Controller to Display information of every pandit
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async(req, res) => {
     const pandits = await Pandit.find({ role: 'pandit' }).select('-password') //removes password
 
     res.render('../views/pandits.ejs', { pandits });
@@ -16,7 +16,7 @@ const getAllUsers = async (req, res) => {
 // THIS ROUTE NEEDS TO BE PROTECTED AND THE USER CAN ONLY ACCESS IF LOGGED IN
 // SO PLEASE ADD LOGIN MIDDLEWARE
 
-const panditProfile = async (req, res) => {
+const panditProfile = async(req, res) => {
     const { id } = req.params;
 
     const pandit = await Pandit.findById(id);
@@ -24,7 +24,7 @@ const panditProfile = async (req, res) => {
     res.render('../views/panditprofile.ejs', { pandit })
 }
 
-const searchPandit = async (req, res) => {
+const searchPandit = async(req, res) => {
 
     const { location, pooja } = req.body
 
@@ -37,30 +37,30 @@ const searchPandit = async (req, res) => {
 
 // Gets the User Info
 
-const getSingleUser = async (req, res) => {
+const getSingleUser = async(req, res) => {
     const user = await Pandit.findOne({ _id: req.params.id }).select('-password')
 
     const pandit = await Pandit.findOne({ _id: panditId }.populate('reviews'))
 
-    // if (!user) {
-    //     throw new CustomError.NotFoundError(`No user with ID : ${req.params.id} `)
-    // }
+    if (!user) {
+        throw new CustomError.NotFoundError(`No user with ID : ${req.params.id} `)
+    }
 
     res.render('../views/pandits.ejs')
-    // res.status(StatusCodes.OK).json({ user })
+        // res.status(StatusCodes.OK).json({ user })
 }
 
 
 // To Display current user
 
-const showCurrentUser = async (req, res) => {
+const showCurrentUser = async(req, res) => {
     res.status(StatusCodes.OK).json({ user: req.user })
 }
 
 
 // Updates user's information (except password and role values) by fetching name and email
 
-const updateUser = async (req, res) => {
+const updateUser = async(req, res) => {
     const { email, name } = req.body
     if (!email || !name) {
         throw new CustomError.BadRequestError('Please, provide all values')
@@ -93,7 +93,7 @@ const updateUser = async (req, res) => {
 
 //Update user's password
 
-const updateUserPassword = async (req, res) => {
+const updateUserPassword = async(req, res) => {
     const { oldPassword, newPassword } = req.body
     if (!oldPassword || !newPassword) {
         throw new CustomError.BadRequestError('Please provide both values')
@@ -112,7 +112,7 @@ const updateUserPassword = async (req, res) => {
     res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated' })
 }
 
-const deleteUser = async (req, res) => {
+const deleteUser = async(req, res) => {
     const { id: panditId } = req.params
 
     const pandit = await Pandit.findOne({ _id: panditId }).select('-password');
